3ca2a863a6018d45282f893c47a822f145d93bf3
annajurys <ania.jurys@gmail.com>
2019-10-17 21:08:18 +0200

    Add result.fxml and ResultController

---
+++ src/project/ie/ResultController.java
@@ -0,0 +1,26 @@
+package project.ie;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ResultController implements Initializable {
+
+    private BorderPane borderPane;
+
+    protected void initVariables(BorderPane borderPane) { this.borderPane = borderPane; }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+}
